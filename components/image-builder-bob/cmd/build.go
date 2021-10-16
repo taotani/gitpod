971a6b2c977ca033108b370a490cf120ed933821
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-10-16 16:26:17 -0300

    build

--- components/image-builder-bob/cmd/build.go
+++ components/image-builder-bob/cmd/build.go
@@ -5,11 +5,12 @@
 package cmd
 
 import (
-	"io/ioutil"
+	"context"
 	"os"
 	"time"
 
 	"github.com/gitpod-io/gitpod/image-builder/bob/pkg/builder"
+	"github.com/moby/buildkit/util/bklog"
 
 	log "github.com/gitpod-io/gitpod/common-go/log"
 	"github.com/spf13/cobra"
@@ -20,7 +21,7 @@ var buildCmd = &cobra.Command{
 	Use:   "build",
 	Short: "Runs the image build and is configured using environment variables (see pkg/builder/config.go for details)",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Init("bob", "", true, false)
+		log.Init("bob", "", true, true)
 		log := log.WithField("command", "build")
 
 		t0 := time.Now()
@@ -28,16 +29,10 @@ var buildCmd = &cobra.Command{
 			log.Fatal("must run as root")
 		}
 
-		defer func() {
-			time.Sleep(10 * time.Second)
-			content, err := ioutil.ReadFile("/workspace/.gitpod/prebuild-log-0")
-			if err != nil {
-				log.WithError(err).Error("unexpected error reading prebuild log")
-			}
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-			log.WithField("log", string(content)).Info("build log")
-			time.Sleep(5 * time.Minute)
-		}()
+		bklog.WithLogger(ctx, log)
 
 		// give the headless listener some time to attach
 		time.Sleep(1 * time.Second)
--- components/image-builder-bob/pkg/builder/builder.go
+++ components/image-builder-bob/pkg/builder/builder.go
@@ -6,24 +6,17 @@ package builder
 
 import (
 	"context"
-	"errors"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/gitpod-io/gitpod/common-go/log"
-	"github.com/google/go-containerregistry/pkg/authn"
-	"github.com/google/go-containerregistry/pkg/crane"
 
-	"github.com/containerd/console"
-	"github.com/containerd/containerd/reference"
 	"github.com/moby/buildkit/client"
-	"github.com/moby/buildkit/session"
-	"github.com/moby/buildkit/util/progress/progressui"
-	"golang.org/x/sync/errgroup"
 	"golang.org/x/xerrors"
 )
 
@@ -91,202 +84,73 @@ func (b *Builder) buildBaseLayer(ctx context.Context, cl *client.Client) error {
 
 	log.Info("building base image")
 
-	var sess []session.Attachable
-	if baselayerAuth := b.Config.BaseLayerAuth; baselayerAuth != "" {
-		auth, err := newAuthProviderFromEnvvar(baselayerAuth)
-		if err != nil {
-			return xerrors.Errorf("invalid base layer authentication: %w", err)
-		}
-		sess = append(sess, auth)
-	}
-
-	contextdir := b.Config.ContextDir
-	if contextdir == "" {
-		contextdir = "."
-	}
-	solveOpt := client.SolveOpt{
-		Frontend: "dockerfile.v0",
-		FrontendAttrs: map[string]string{
-			"filename": filepath.Base(b.Config.Dockerfile),
-		},
-		LocalDirs: map[string]string{
-			"context":    contextdir,
-			"dockerfile": filepath.Dir(b.Config.Dockerfile),
-		},
-		Session:      sess,
-		CacheImports: b.Config.LocalCacheImport(),
-	}
-
-	eg, ectx := errgroup.WithContext(ctx)
-	ch := make(chan *client.SolveStatus)
-	eg.Go(func() error {
-		_, err := cl.Solve(ectx, nil, solveOpt, ch)
-		if err != nil {
-			// buildkit errors are wrapped to contain the stack - that does not make for a pretty
-			// sight when printing it to the user.
-			if u := errors.Unwrap(err); u != nil {
-				return u
-			}
-
-			return err
-		}
-		return nil
-	})
-	eg.Go(func() error {
-		var c console.Console
-		return progressui.DisplaySolveStatus(ectx, "", c, os.Stdout, ch)
-	})
-	err = eg.Wait()
-	if err != nil {
-		return err
-	}
-
-	// First we built the base, now we push the image by building it again.
-	// We can't do this in one go because we cannot separate authentication for pull and push.
-	// However we want separate authentication for pulling the FROM of base image builds and pushing
-	// the built base images.
-	solveOpt.Exports = []client.ExportEntry{
-		{
-			Type: "image",
-			Attrs: map[string]string{
-				"name": b.Config.BaseRef,
-				"push": "true",
-			},
-		},
-	}
-	if lauth := b.Config.WorkspaceLayerAuth; lauth != "" {
-		auth, err := newAuthProviderFromEnvvar(lauth)
-		if err != nil {
-			return xerrors.Errorf("invalid gp layer authentication: %w", err)
-		}
-		solveOpt.Session = []session.Attachable{auth}
-	}
-	eg, ectx = errgroup.WithContext(ctx)
-	ch = make(chan *client.SolveStatus)
-	eg.Go(func() error {
-		_, err := cl.Solve(ectx, nil, solveOpt, ch)
-		if err != nil {
-			// buildkit errors are wrapped to contain the stack - that does not make for a pretty
-			// sight when printing it to the user.
-			if u := errors.Unwrap(err); u != nil {
-				return u
-			}
-
-			return err
-		}
-		return nil
-	})
-	eg.Go(func() error {
-		var c console.Console
-		return progressui.DisplaySolveStatus(ectx, "", c, os.Stdout, ch)
-	})
-	err = eg.Wait()
-	if err != nil {
-		return err
-	}
-
-	log.Info("base image done")
-	return err
+	log.WithField("buildConfig", b.Config).Info("build config")
+	return buildImage(b.Config.ContextDir, b.Config.Dockerfile, b.Config.WorkspaceLayerAuth, b.Config.BaseRef, b.Config.TargetRef)
 }
 
 func (b *Builder) buildWorkspaceImage(ctx context.Context, cl *client.Client) (err error) {
-	// Workaround: buildkit/containerd currently does not support pushing multi-image builds
-	//             with some registries, e.g. gcr.io. Until https://github.com/containerd/containerd/issues/5978
-	//             is resolved, we'll manually copy the image.
-	var craneOpts []crane.Option
-	if gplayerAuth := b.Config.WorkspaceLayerAuth; gplayerAuth != "" {
-		authorizer, err := NewAuthorizerFromEnvVar(gplayerAuth)
+	log.WithField("buildConfig", b.Config).Info("build config")
+	return buildImage(b.Config.ContextDir, b.Config.Dockerfile, b.Config.WorkspaceLayerAuth, b.Config.BaseRef, b.Config.TargetRef)
+}
+
+func buildImage(contextDir, dockerfile, authLayer, source, target string) error {
+	if authLayer != "" {
+		_ = os.MkdirAll(filepath.Join(os.Getenv("HOME"), ".docker"), 0644)
+		dockerConfig := filepath.Join(os.Getenv("HOME"), ".docker", "config.json")
+
+		data, err := strconv.Unquote(authLayer)
 		if err != nil {
 			return err
 		}
 
-		tref, err := reference.Parse(b.Config.TargetRef)
-		if err != nil {
-			return err
-		}
-		user, pass, err := authorizer.Authorize(tref.Hostname())
-		if err != nil {
-			return err
-		}
+		_ = os.WriteFile(dockerConfig, []byte(data), 0644)
+		defer os.Remove(dockerConfig)
 
-		craneOpts = append(craneOpts, crane.WithAuth(authn.FromConfig(authn.AuthConfig{
-			Username: user,
-			Password: pass,
-		})))
+		c, _ := ioutil.ReadFile(dockerConfig)
+		log.Println(string(c))
 	}
 
-	return crane.Copy(b.Config.BaseRef, b.Config.TargetRef, craneOpts...)
+	contextdir := contextDir
+	if contextdir == "" {
+		contextdir = "."
+	}
 
-	// // Note: buildkit does not handle/export image config by default. That's why we need
-	// //       to download it ourselves and explicitely export it.
-	// //       See https://github.com/moby/buildkit/issues/2362 for details.
-	// var sess []session.Attachable
-	// if gplayerAuth := b.Config.WorkspaceLayerAuth; gplayerAuth != "" {
-	// 	auth, err := newAuthProviderFromEnvvar(gplayerAuth)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	sess = append(sess, auth)
+	buildctlArgs := []string{
+		"build",
+		"--progress=plain",
+		"--frontend=dockerfile.v0",
+		"--output=type=image,name=" + target + ",push=true",
+		"--export-cache=type=inline",
+	}
 
-	// 	authorizer, err := newDockerAuthorizerFromEnvvar(gplayerAuth)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	resolver = docker.NewResolver(docker.ResolverOptions{
-	// 		Authorizer: authorizer,
-	// 	})
-	// } else {
-	// 	resolver = docker.NewResolver(docker.ResolverOptions{})
-	// }
+	if _, err := os.Stat(dockerfile); os.IsNotExist(err) {
+		log.WithError(err).Errorf("dockerfile %v does not exists", dockerfile)
+		buildctlArgs = append(buildctlArgs,
+			"--opt source="+source,
+		)
+	} else {
+		buildctlArgs = append(buildctlArgs,
+			"--local=context="+contextdir,
+			"--local=dockerfile="+filepath.Dir(dockerfile),
+			"--opt=filename="+filepath.Base(dockerfile),
+		)
+	}
 
-	// platform := specs.Platform{OS: "linux", Architecture: "amd64"}
-	// _, cfg, err := imageutil.Config(ctx, b.Config.BaseRef, resolver, contentutil.NewBuffer(), nil, &platform)
-	// if err != nil {
-	// 	return err
-	// }
-	// state, err := llb.Image(b.Config.BaseRef).WithImageConfig(cfg)
-	// if err != nil {
-	// 	return err
-	// }
+	log.WithField("buildctlArgs", buildctlArgs).Info("build args")
 
-	// def, err := state.Marshal(ctx, llb.Platform(platform))
-	// if err != nil {
-	// 	return err
-	// }
+	buildctlCmd := exec.Command("buildctl", buildctlArgs...)
+	buildctlCmd.Stderr = os.Stderr
+	buildctlCmd.Stdout = os.Stdout
 
-	// // TODO(cw):
-	// // buildkit does not support setting raw annotations yet (https://github.com/moby/buildkit/issues/1220).
-	// // Once it does, we should set org.opencontainers.image.base.name as defined in https://github.com/opencontainers/image-spec/blob/main/annotations.md
+	if err := buildctlCmd.Start(); err != nil {
+		return err
+	}
 
-	// solveOpt := client.SolveOpt{
-	// 	Exports: []client.ExportEntry{
-	// 		{
-	// 			Type: "image",
-	// 			Attrs: map[string]string{
-	// 				"name":                  b.Config.TargetRef,
-	// 				"push":                  "true",
-	// 				"containerimage.config": string(cfg),
-	// 			},
-	// 		},
-	// 	},
-	// 	Session:      sess,
-	// 	CacheImports: b.Config.LocalCacheImport(),
-	// }
+	if err := buildctlCmd.Wait(); err != nil {
+		return err
+	}
 
-	// eg, ctx := errgroup.WithContext(ctx)
-	// ch := make(chan *client.SolveStatus)
-	// eg.Go(func() error {
-	// 	_, err := cl.Solve(ctx, def, solveOpt, ch)
-	// 	if err != nil {
-	// 		return xerrors.Errorf("cannot build Gitpod layer: %w", err)
-	// 	}
-	// 	return nil
-	// })
-	// eg.Go(func() error {
-	// 	var c console.Console
-	// 	return progressui.DisplaySolveStatus(ctx, "", c, os.Stdout, ch)
-	// })
-	// return eg.Wait()
+	return nil
 }
 
 func waitForBuildContext(ctx context.Context) error {
